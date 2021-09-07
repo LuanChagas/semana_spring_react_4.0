671b7dac41055440364d7c61162d55a79f10ed53
Luan Chagas <luanchagas@hotmail.com>
2021-09-06 22:40:41 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
+import BarChat from "components/BarChat";
 import DataTable from "components/DataTable";
+import DonutChat from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/Navbar/Index";
-
-
 
 function App() {
   return (
     <>
-      <NavBar/>
-    
-    <DataTable/>
-      <Footer/>
-       </>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChat />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center">Taxa de sucesso (%)</h5>
+            <DonutChat />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
