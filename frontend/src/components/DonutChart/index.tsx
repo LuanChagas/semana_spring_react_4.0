332d595b3888bbdffacb67cfa87093ddc0c1c3ce
Luan Chagas <luanchagas@hotmail.com>
2021-09-10 16:02:21 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,44 +2,44 @@ import axios from "axios"
 import Chart from "react-apexcharts"
 import BASE_URL from "utils/request"
 import { SaleSum } from "types/sale"
+import { useEffect, useState } from "react"
 type ChartData = {
     labels: string[];
     series: number[];
 }
-const DonutChat
-    = () => {
-        let chartData: ChartData = { labels: [], series: [] }
+const DonutChat = () => {
 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
+
+
+    useEffect(() => {
         axios.get(`${BASE_URL}sales/amount-by-seller`).then(
             response => {
                 const data = response.data as SaleSum[];
                 const MyLabels = data.map(x => x.sellerName);
                 const MySeries = data.map(x => x.sum);
 
-                chartData = { labels: MyLabels, series: MySeries }
-                console.log(chartData)
+                setChartData({ labels: MyLabels, series: MySeries })
             }
-        )
-
-        const mockData = {
-            series: [477138, 499928, 444867, 220426, 473088],
-            labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        }
-
-        const options = {
-            legend: {
-                show: true
-            }
-        }
-        return (
-            <Chart
-                options={{ ...options, labels: mockData.labels }}
-                series={mockData.series}
-                type="donut"
-                height="240"
-            />
         );
+
+    }, [])
+
+
+    const options = {
+        legend: {
+            show: true
+        }
     }
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
+}
 
 export default DonutChat
     ;
